0970ac9aea728f81ac06742873c470323ef2d641
Robson Sobral <robson@oficiodesign.com>
2017-09-08 18:05:59 -0300

    Make eslint happier

--- _buildScripts/dataContent.js
+++ _buildScripts/dataContent.js
@@ -1,61 +1,66 @@
-var https = require('https');
-var fs = require('fs');
 
-const DOMAIN = process.env['NODE_ENV'] === 'production'
-			? 'https://api.2017.deolhonasmetas.org.br'
-			: 'https://dapidonm.eokoe.com';
+const https = require('https');
+const fs = require('fs');
 
-var sourcesAndDests = [
+const DOMAIN = process.env.NODE_ENV === 'production' ? 'https://api.2017.deolhonasmetas.org.br' : 'https://dapidonm.eokoe.com';
+
+const sourcesAndDests = [
 	{
-		url:           '/api/region',
-		dataDest:      './data/districts.json',
-		contentFolder: './content/distritos'
-	}
+		url: '/api/region',
+		dataDest: './data/districts.json',
+		contentFolder: './content/distritos',
+	},
 ];
 
-var currentContentFolder;
+let currentContentFolder;
 
 // @see https://gist.github.com/mathewbyrne/1280286#gistcomment-2100112
-var slugify = function (str) {
-	const from  = "ąàáäâãåæćçęęèéëêìíïîłńòóöôõøśùúüûñçżź";
-	const to    = "aaaaaaaacceeeeeeiiiilnoooooosuuuunczz";
-	const regex = new RegExp('[' + from.replace(/([.*+?^=!:${}()|[\]\/\\])/g, '\\$1') + ']', 'g');
+function slugify(str) {
+	const from = 'ąàáäâãåæćçęęèéëêìíïîłńòóöôõøśùúüûñçżź';
+	const to = 'aaaaaaaacceeeeeeiiiilnoooooosuuuunczz';
+	let regex = from.replace(/([.*+?^=!:${}()|[\]/\\])/g, '\\$1');
+
+	regex = new RegExp(`[${regex}]`, 'g');
 
 	if (str === null) return '';
 
-	str = String(str).toLowerCase().replace(regex, function(c) {
-		return to.charAt(from.indexOf(c)) || '-';
-	});
+	return String(str)
+		.toLowerCase()
+		.replace(regex, c => to.charAt(from.indexOf(c)) || '-')
+		.replace(/[^\w\s-]/g, '')
+		.replace(/([A-Z])/g, '-$1')
+		.replace(/[-_\s]+/g, '-')
+		.toLowerCase();
+}
 
-	return str.replace(/[^\w\s-]/g, '').replace(/([A-Z])/g, '-$1').replace(/[-_\s]+/g, '-').toLowerCase();
-};
+function userException(message) {
+	this.message = message;
+	this.name = 'userException';
+}
 
+function savePages() {
+	const fileContent = fs.readFileSync(this.path, 'utf-8');
+	const jsonElements = JSON.parse(fileContent).region;
+	const pageList = [];
 
-var savePages = function() {
-	let fileContent = fs.readFileSync(this.path, 'utf-8');
+	jsonElements.forEach((page) => {
+		const filename = `${currentContentFolder}/${slugify(page.name)}.md`;
 
-	let jsonElements = JSON.parse(fileContent).region;
-
-	let pageList = [];
-
-	for (let page of jsonElements){
-		let filename = currentContentFolder+'/'+slugify(page.name)+'.md';
-
-		if (pageList.indexOf(filename) !== -1){
-			throw `${filename} already exists.`;
+		if (pageList.indexOf(filename) !== -1) {
+			throw new userException(`${filename} already exists.`);
 		}
 
-		let contentFile = fs.createWriteStream(filename, {'flags': 'w'});
+		const contentFile = fs.createWriteStream(filename, { flags: 'w' });
 
-		contentFile.on("open", () => {
+		contentFile.on('open', () => {
 			console.log(`Writing ${filename}`);
 		});
 
-		contentFile.on("close", () => {
-			console.log(`${filename} saved.`)
+		contentFile.on('close', () => {
+			console.log(`${filename} saved.`);
 		});
 
-		let frontMatter = `---
+		const frontMatter = `---
 date: ${new Date().toISOString()}
 draft: false
 title: ${page.name}
@@ -68,41 +73,40 @@ id: ${page.id}
 		pageList[pageList.length] = filename;
 
 		contentFile.close();
-	}
+	});
 }
 
-var download = function(url, dest, cb) {
-	var file = fs.createWriteStream(dest);
 
-	var request = https.get(url, function(response) {
+function download(url, dest, cb) {
+	const file = fs.createWriteStream(dest);
+
+	const request = https.get(url, (response) => {
 		// check if response is success
 		if (response.statusCode !== 200) {
-			if (cb) cb('Response status was ' + response.statusCode);
+			if (cb) cb(`Response status was ${response.statusCode}`);
 		}
 
 		response.pipe(file);
 
-		file.on('finish', function() {
+		file.on('finish', () => {
 			file.close(cb); // close() is async, call cb after close completes.
 			console.log(`${url} saved as ${dest}`);
 		});
 	});
 
-	request.on('error', function(err) { // Handle errors
+	request.on('error', (err) => { // Handle errors
 		fs.unlink(dest); // Delete the file async. (But we don't check the result)
 		if (cb) cb(err.message);
 	});
 
-	file.on('error', function(err) { // Handle errors
+	file.on('error', (err) => { // Handle errors
 		fs.unlink(dest); // Delete the file async. (But we don't check the result)
 		if (cb) cb(err.message);
 	});
-};
+}
 
-for (let file of sourcesAndDests) {
+sourcesAndDests.forEach((file) => {
 	currentContentFolder = file.contentFolder;
 
 	download(`${DOMAIN}${file.url}`, file.dataDest, savePages);
-}
-
-
+});
