c4fb7876ff2cf5e413987c42ff5cf0b8f3373c1d
sobral <sobral@eokoe.com>
2017-10-27 19:14:20 -0200

    Add javascript helpers

---
+++ _source/javascripts/helpers/gradientSteps.js
@@ -0,0 +1,24 @@
+export default function gradientSteps(firstColor, lastColor, steps = 1) {
+	let cMax;
+	let cMin;
+	let j = 1;
+
+	const r = [];
+
+	while (j <= steps) {
+		let i = 0;
+		const c = [];
+
+		while (i < firstColor.length) {
+			cMax = Math.max(firstColor[i], lastColor[i]);
+			cMin = Math.min(firstColor[i], lastColor[i]);
+			c.push(Math.round(((cMax - cMin) / ((steps + 1) * j)) + cMin));
+			i += 1;
+		}
+
+		r.push(c);
+		j += 1;
+	}
+
+	return r;
+}
---
+++ _source/javascripts/helpers/index.js
@@ -0,0 +1,4 @@
+import gradientSteps from './gradientSteps';
+import throwIfMissing from './throwIfMissing';
+
+export { gradientSteps, throwIfMissing };
