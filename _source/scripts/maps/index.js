4ec8169a8ade1b4bb1fc89cb0a79d83f3529e266
sobral <sobral@eokoe.com>
2017-10-30 18:37:57 -0200

    Custom load regions / subprefectures on maps
    DONM-173 #time 5h 30m #review

--- _source/scripts/maps/index.js
+++ _source/scripts/maps/index.js
@@ -7,16 +7,12 @@ import * as panel from './customInfoPanel';
 
 import drawPolygon from './drawPolygon';
 
+import apiSources from '../apiSources.json';
+
 let map;
 
 export default function initMap() {
 	const mapElement = document.getElementById('map');
-
-	let areasToDraw = 'all';
-
-	if (mapElement.hasAttribute('data-district')) {
-		areasToDraw = mapElement.getAttribute('data-district').split(' ').map(item => parseInt(item, 10));
-	}
 
 	map = new google.maps.Map(mapElement, {
 		center: { lat: -23.55, lng: -46.633333 },
@@ -24,107 +20,124 @@ export default function initMap() {
 		styles: mapStyles,
 	});
 
-	const myHeaders = new Headers();
+	function getArrayBounds(polyArray) {
+		const bounds = new google.maps.LatLngBounds();
+		let path;
+		let paths;
 
-	myHeaders.append('Content-Type', 'application/json');
+		for (let polys = 0; polys < polyArray.length; polys += 1) {
+			paths = polyArray[polys].getPaths();
+			for (let l = 0; l < paths.getLength(); l += 1) {
+				path = paths.getAt(l);
 
-	const myInit = {
-		method: 'GET',
-		headers: myHeaders,
-		mode: 'cors',
-		cache: 'default',
-	};
-
-	const myRequest = new Request('/dist/data/districts.json');
-
-	const rootElement = 'regions';
-
-	fetch(myRequest, myInit)
-		.then((response) => {
-			const contentType = response.headers.get('content-type');
-
-			if (contentType && contentType.includes('application/json')) {
-				return response.json();
+				for (let ii = 0; ii < path.getLength(); ii += 1) {
+					bounds.extend(path.getAt(ii));
+				}
 			}
+		}
+		return bounds;
+	}
 
-			throw new TypeError("Oops, we haven't got JSON!");
-		})
-		.then((areasList) => {
-			function getArrayBounds(polyArray) {
-				const bounds = new google.maps.LatLngBounds();
-				let path;
-				let paths;
+	if (mapElement.hasAttribute('data-to-draw')) {
+		const toDraw = mapElement.getAttribute('data-to-draw').split(' ');
 
-				for (let polys = 0; polys < polyArray.length; polys += 1) {
-					paths = polyArray[polys].getPaths();
-					for (let l = 0; l < paths.getLength(); l += 1) {
-						path = paths.getAt(l);
+		const polygonsBounds = apiSources
+			.filter(endPoint => toDraw.indexOf(endPoint.jsonRootElement) !== -1)
+			.map((endPointData) => {
+				const rootElement = endPointData.jsonRootElement;
 
-						for (let ii = 0; ii < path.getLength(); ii += 1) {
-							bounds.extend(path.getAt(ii));
+				const myHeaders = new Headers();
+
+				myHeaders.append('Content-Type', 'application/json');
+
+				const myInit = {
+					method: 'GET',
+					headers: myHeaders,
+					mode: 'cors',
+					cache: 'default',
+				};
+
+				const myRequest = new Request(endPointData.dataDest.replace('./static', ''));
+
+				return fetch(myRequest, myInit)
+					.then((response) => {
+						const contentType = response.headers.get('content-type');
+
+						if (contentType && contentType.includes('application/json')) {
+							return response.json();
 						}
-					}
-				}
-				return bounds;
-			}
 
-			let polygonsToDraw = areasList[rootElement];
+						throw new TypeError("Oops, we haven't got JSON!");
+					})
+					.then((areasList) => {
+						let polygonsToDraw = areasList[rootElement];
 
-			if (areasToDraw !== 'all') {
-				polygonsToDraw = polygonsToDraw.filter(x => areasToDraw.indexOf(x.id) !== -1);
-			}
+						if (mapElement.hasAttribute(`data-${rootElement}-to-draw`)) {
+							const areasToDraw = mapElement.getAttribute(`data-${rootElement}-to-draw`).split(' ')
+								.map(item => parseInt(item, 10));
+							polygonsToDraw = polygonsToDraw.filter(x => areasToDraw.indexOf(x.id) !== -1);
+						}
 
-			const polygons = [];
+						const polygons = [];
 
-			for (let i = 0; i < polygonsToDraw.length; i += 1) {
-				const polygon = polygonsToDraw[i];
+						for (let i = 0; i < polygonsToDraw.length; i += 1) {
+							const polygon = polygonsToDraw[i];
 
-				if (polygon.geo_json !== null) {
-					const geoJSON = JSON.parse(polygon.geo_json);
+							if (polygon.geo_json !== null) {
+								const geoJSON = JSON.parse(polygon.geo_json);
 
-					const drawnPolygon = drawPolygon(geoJSON.coordinates, map);
+								const drawnPolygon = drawPolygon(geoJSON.coordinates, map);
 
-					google.maps.event.addListener(drawnPolygon, 'mouseover', () => {
-						drawnPolygon.setOptions(polygonStyles.toggle);
+								google.maps.event.addListener(drawnPolygon, 'mouseover', () => {
+									drawnPolygon.setOptions(polygonStyles.toggle);
+								});
+
+								google.maps.event.addListener(drawnPolygon, 'mouseout', () => {
+									drawnPolygon.setOptions(polygonStyles.initial);
+								});
+
+								if (mapElement.hasAttribute('data-info-panel-for')) {
+									const infoPanelFor = mapElement.getAttribute('data-info-panel-for').split(' ');
+
+									if (infoPanelFor.indexOf(rootElement) !== -1) {
+										drawnPolygon.panelContent = `${polygon.name}`;
+
+										if (drawnPolygon.panelContent !== null) {
+											google.maps.event.addListener(drawnPolygon, 'mouseover', (e) => {
+												panel.insertInfoPanel(e, drawnPolygon.panelContent);
+											});
+
+											google.maps.event.addListener(drawnPolygon, 'mouseout', () => {
+												panel.deleteInfoPanel();
+											});
+
+											google.maps.event.addListener(drawnPolygon, 'mousemove', (e) => {
+												panel.moveInfoPanel(e);
+											});
+										}
+									}
+								}
+
+								polygons.push(drawnPolygon);
+							}
+						}
+						return getArrayBounds(polygons) || null;
+					})
+					.catch((error) => {
+						throw new TypeError(error);
 					});
+			});
 
-					google.maps.event.addListener(drawnPolygon, 'mouseout', () => {
-						drawnPolygon.setOptions(polygonStyles.initial);
-					});
-
-					drawnPolygon.panelContent = `${polygon.name} / ${polygon.subprefecture.name}`;
-
-					if (drawnPolygon.panelContent !== null) {
-						google.maps.event.addListener(drawnPolygon, 'mouseover', (e) => {
-							panel.insertInfoPanel(e, drawnPolygon.panelContent);
-						});
-
-						google.maps.event.addListener(drawnPolygon, 'mouseout', () => {
-							panel.deleteInfoPanel();
-						});
-
-						google.maps.event.addListener(drawnPolygon, 'mousemove', (e) => {
-							panel.moveInfoPanel(e);
-						});
-					}
-
-					polygons.push(drawnPolygon);
-				}
+		Promise.all(polygonsBounds).then((results) => {
+			if (results[0]) {
+				map.fitBounds(results[0]);
 			}
 
-			const polygonsBounds = getArrayBounds(polygons) || null;
-
-			if (polygonsBounds) {
-				map.fitBounds(polygonsBounds);
-
-				if (mapElement.getAttribute('data-zoom') === '+1') {
-					google.maps.event.addListenerOnce(map, 'bounds_changed', () => {
-						map.setZoom(map.getZoom() + 1);
-					});
-				}
+			if (mapElement.getAttribute('data-zoom') === '+1') {
+				google.maps.event.addListenerOnce(map, 'bounds_changed', () => {
+					map.setZoom(map.getZoom() + 1);
+				});
 			}
-		})
-		.catch((error) => {
-			throw new TypeError(error);
 		});
+	}
 }
