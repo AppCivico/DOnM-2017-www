2b6c25fcd3097df1633a0097a72b51051c8eeb52
sobral <sobral@eokoe.com>
2017-11-06 12:03:18 -0200

    FIx conflicts

--- _source/scripts/maps/customInfoPanel.js
+++ _source/scripts/maps/customInfoPanel.js
@@ -17,21 +17,22 @@ let coordPropName = null;
 export function insertInfoPanel(event, data) {
 	if (!panelEl && event) {
 		// create the InfoPanel object
-		panelEl = document.createElement('div');
-		panelEl.className = 'custom-info-panel';
 		if (typeof data === 'string') {
+			panelEl = document.createElement('div');
+			panelEl.className = 'custom-info-panel';
 			panelEl.innerHTML = data;
 		} else if (typeof data === 'object' && 'nodeType' in data && data.nodeType === 1 && data.cloneNode) {
-			panelEl.appendChild(data);
+			panelEl = data;
+			panelEl.className += ' custom-info-panel';
 		}
 
 		// get the property which captures the mouse event
 		coordPropName = Object.keys(event).filter(p => event[p] instanceof MouseEvent);
 
 		if (coordPropName[0]) {
-			panelEl.style.position = 'fixed';
-			panelEl.style.top = `${event[coordPropName[0]].clientY + offset.y}px`;
-			panelEl.style.left = `${event[coordPropName[0]].clientX + offset.x}px`;
+			panelEl.style.position = 'absolute';
+			panelEl.style.top = `${event[coordPropName[0]].clientY + window.scrollY + offset.y}px`;
+			panelEl.style.left = `${event[coordPropName[0]].clientX + window.scrollX + offset.x}px`;
 
 			document.body.appendChild(panelEl);
 		}
@@ -41,8 +42,8 @@ export function insertInfoPanel(event, data) {
 export function moveInfoPanel(event) {
 	if (panelEl && event && coordPropName[0]) {
 		// position it
-		panelEl.style.top = `${event[coordPropName[0]].clientY + offset.y}px`;
-		panelEl.style.left = `${event[coordPropName[0]].clientX + offset.x}px`;
+		panelEl.style.top = `${event[coordPropName[0]].clientY + window.scrollY + offset.y}px`;
+		panelEl.style.left = `${event[coordPropName[0]].clientX + window.scrollX + offset.x}px`;
 	}
 }
 
