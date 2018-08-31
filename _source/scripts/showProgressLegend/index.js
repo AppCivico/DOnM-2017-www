1d3f60acb2f76ddc3975ae1ca9d843b2a95b87a8
edu ruiz <eduardo.ruiz@eokoe.com>
2018-08-31 13:06:12 -0300

    Adjust progress legend to hide on mouseleave

--- _source/scripts/showProgressLegend/index.js
+++ _source/scripts/showProgressLegend/index.js
@@ -9,6 +9,16 @@ export default function initProgressHover() {
 		document.querySelector('.js-progress-more-info p').textContent = event.target.dataset.moreInfo;
 	}
 
+	function hideMe() {
+		document.querySelector('.js-progress-more-info p').textContent = '';
+	}
+
+	$progressHover.forEach(button => button.addEventListener(
+		'mouseleave',
+		hideMe,
+		false,
+	));
+
 	return $progressHover.forEach(button => button.addEventListener(
 		'mouseover',
 		showMe,
